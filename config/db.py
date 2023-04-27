fbe24562926bfbc276189f155f3df7518a719535
phelipestation <phelipe.gomes@kyndryl.com>
2023-04-27 14:58:26 -0300

    /EDIT - add docstring in db class

--- config/db.py
+++ config/db.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine, MetaData
 
 class Database:
+    """A class representing a database connection."""
     def __init__(self, model, user, password, url, database):
+        """
+        Initializes a new database connection.
+
+        Args:
+            model (str): The database model to use.
+            user (str): The username to connect to the database.
+            password (str): The password to connect to the database.
+            url (str): The URL of the database server.
+            database (str): The name of the database to connect to.
+        """
         self.model = model
         self.user = user
         self.password = password
