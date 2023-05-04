ed0cfb20a95a6e2e65e48cf735856e6e21ff21b7
phelipegomes <phelipecedaspy@gmail.com>
2023-05-04 17:10:20 -0300

    /FIX - cleaning the code

--- routes/products.py
+++ routes/products.py
@@ -15,7 +15,8 @@ def create_product(product: Product):
     try:
         # Insert the new product into the database
         result = conn.execute(products.insert().values(new_item))
-        return 'Product created successfully.', conn.execute(products.select().where(products.c.id == result.lastrowid)).first()
+        product_res = conn.execute(products.select().where(products.c.id == result.lastrowid)).first()
+        return 'Product created successfully.', product_res
     except Exception as e:
         print(f'Error creating product: {e}')
         return 'Error creating product'
@@ -23,21 +24,19 @@ def create_product(product: Product):
     # Commiting changes
     conn.commit()
 
-
 # Route to search all products
 @route_product.get('/products', tags=["Products"])
 def get_products():
     return conn.execute(products.select()).fetchall()
 
-
 # Route to search products by ID
 @route_product.get('/products/{id}', tags=["Search products by ID"])
 def get_product(id: int):
-    
     # Getting the product by ID of the database
     result = conn.execute(products.select().where(
         products.c.id == id)).first()
     
+    # Check if product already exists
     if(not result):
         raise HTTPException(status_code=404, detail="Item not found")   
     return result
@@ -55,7 +54,8 @@ def update_product(id: int, product: Product):
         price=product.price, quantity=product.quantity,
         color=product.color, serial=product.serial
     ).where(products.c.id == id))
-    return 'Product updated successfully! The new properties is:', conn.execute(products.select().where(products.c.id == id)).first()
+    product_res = conn.execute(products.select().where(products.c.id == id)).first()
+    return 'Product updated successfully! The new properties is:', product_res
 
     # Commiting changes
     conn.commit()
